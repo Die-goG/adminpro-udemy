e0ed7cf163c11d28aa454c5df3db48b3a0d51e7e
Diego <diego.alvarracin.g@gmail.com>
2020-03-30 11:39:45 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,40 +4,28 @@ import { NgModule } from '@angular/core';
 /*Importamos el archivo app.Routes.ts de en donde estan definidas las Rutas*/
 import { APP_ROUTES } from './app.routes';
 
+/* Modulos*/
+import { PagesModule } from './pages/pages.module';
 
+
+/* Componentes*/
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES    /*Importamos la variable que contiene las rutas, esta variable esta definida en app.routes.ts*/
+    APP_ROUTES,    /*Importamos la variable que contiene las rutas, esta variable esta definida en app.routes.ts*/
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } /*declaramos el nombre que tendra este modulo*/
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,41 +1,17 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { PagesComponent } from './pages/pages.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { RegisterComponent } from './login/register.component';
 
 
 // Aqui definimos las rutas en la variable constante appRoutes
 
 const appRoutes: Routes = [
-    {path: '', component: PagesComponent,
-        children: [
-            {path: 'dashboard', component: DashboardComponent },
-            {path: 'progress', component: ProgressComponent },
-            {path: 'graficas1', component: Graficas1Component },
-            {path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-        ]
-    },
     {path: 'login', component: LoginComponent },
     {path: 'register', component: RegisterComponent },
     {path: '**', component: NopagefoundComponent }
 ];
-
-// const appRoutes: Routes = [
-//     {path: '', component: PagesComponent }, /* path: '' significa sin ruta*/
-//     {path: 'dashboard', component: DashboardComponent },
-//     {path: 'login', component: LoginComponent },
-//     {path: 'register', component: LoginComponent },
-//     {path: 'progress', component: ProgressComponent },
-//     {path: 'graficas1', component: Graficas1Component },
-//     // {path: '', redirectTo: '/dashboard', pathMatch: 'full' }, /* ruta para cuando no exista ninguna ruta */
-//     {path: '**', component: NopagefoundComponent } /* cualquier ruta que no este definida aqui, nos muestra nopagefound */
-// ];
 
 /* vamos a exportar el arreglo Routes que contiene las rutas definidas
 La variable Routes debe estar referenciada en app.Modules.ts*/
